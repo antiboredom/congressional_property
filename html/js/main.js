71fdd649bba282c08db762de5a106d2a69577f65
Sam <splavigne@gmail.com>
2015-03-31 18:41:35 -0400

    more d3-like

--- html/js/main.js
+++ html/js/main.js
@@ -41,29 +41,30 @@ d3.csv('assets/property.csv', function(data){
 
   var html = property_template(data);
   d3.select('#properties-wrapper').html(html);
+  d3.selectAll('.property')
+    .data(data.items)
+    .on('click', function(d){
+      var me = d3.select(this);
+      var embed_url = d.streetview_embed;
+      var img = me.select('.main-image');
+      var h = me.select('.inner').node().getBoundingClientRect().height;
 
-  d3.selectAll('.property').on('click', function(){
-    var me = d3.select(this);
-    var embed_url = me.attr('data-embed');
-    var img = me.select('.main-image');
-    var h = me.select('.inner').node().getBoundingClientRect().height;
+      var iframe = me.select('.inner')
+        .append('iframe')
+        .attr('src', embed_url)
+        .attr('width', '100%')
+        .attr('height', h)
+        .attr('frameborder', '0')
+        .style('display', 'none');
 
-    var iframe = me.select('.inner')
-      .append('iframe')
-      .attr('src', embed_url)
-      .attr('width', '100%')
-      .attr('height', h)
-      .attr('frameborder', '0')
-      .style('display', 'none');
+      iframe.on('load', function(){
+        iframe.style('display', 'block');
+        img.remove();
+      });
 
-    iframe.on('load', function(){
-      iframe.style('display', 'block');
-      img.remove();
+      me.on('click', null)
+
     });
-
-    me.on('click', null)
-
-  });
 });
 
 
