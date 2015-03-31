84e4179975c81e3d419cb928565246919750844a
Sam <splavigne@gmail.com>
2015-03-30 20:30:59 -0400

    migrate to handlebars

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
 d3.csv('property_unique.csv', function(data){
+
+  data = {items: data}
+
   console.log(data)
-  var items = d3.select('#images')
-    .selectAll('a')
-    .data(data)
-    .enter()
-    .append('a')
-    .attr('href', function(d) { return 'https://www.google.com/maps/search/' + d.google_address })
-    .attr('target', '_blank')
 
-  items
-    .append('img')
-    .attr('src', function(d){ return '/images/' + d.pfid + '.jpg' })
-    .attr('title', function(d){ console.log(d.name); return d.name + ' :: ' + d.original_address + ' :: ' + d.google_address });
+  var template_source = d3.select("#property-template").html();
+  var property_template = Handlebars.compile(template_source);
 
-  items
-    .append('div')
-    .attr('class', 'overlay')
+  var html = property_template(data)
+  d3.select('#properties-wrapper').html(html) 
+
+  // var items = d3.select('#images')
+  //   .selectAll('a')
+  //   .data(data)
+  //   .enter()
+  //   .append('a')
+  //   .attr('href', function(d) { return 'https://www.google.com/maps/search/' + d.google_address })
+  //   .attr('target', '_blank')
+  //
+  // items
+  //   .append('img')
+  //   .attr('src', function(d){ return '/images/' + d.pfid + '.jpg' })
+  //   .attr('title', function(d){ console.log(d.name); return d.name + ' :: ' + d.original_address + ' :: ' + d.google_address });
+  //
+  // items
+  //   .append('div')
+  //   .attr('class', 'overlay')
 
 })
